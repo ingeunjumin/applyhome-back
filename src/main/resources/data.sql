39a582020d4ae46a0b5f961c865efdef5dc22ea4
hyunsangwon <hyunsangwon93@gmail.com>
2022-06-23 16:20:15 +0900

    feat : init.sql 테이블 추가, data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,52 @@
+-- 지하철
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","용문역","36.3382573","127.3933146");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","오룡역","36.3285736","127.4050512");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","탄방역","36.3455219","127.3845911");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","노은역","36.3741533","127.3178326");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","지족역","36.3841125","127.3194957");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","반석역","36.3921237","127.3146216");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","중앙로역","36.328666","127.4258344");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","갈마역","36.3575553","127.3727526");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","월평역","36.358277","127.3643454");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","대전역","36.3315486","127.4331202");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","서대전네거리역","36.3224189","127.4125265");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","시청역","36.3516794","127.3867376");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","중구청역","36.3248328","127.4196254");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","정부청사역","36.3574457","127.3811436");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","구암역","36.3566869","127.3305531");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","판암역","36.3169023","127.458261");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","현충원역","36.359042","127.3214518");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","갑천역","36.3546829","127.3545809");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","대동역","36.3295394","127.4428474");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","유성온천역","36.3537017","127.3415532");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","신흥역","36.3197597","127.4484742");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("지하철","월드컵경기장역","36.3668713","127.3178403");
+-- 터미널
+insert into business_area(business_type,business_name,latitude,longitude)
+values("터미널","대전복합터미널","36.3513074","127.4375009");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("터미널","대전유성시외터미널","36.3562543","127.3304702");
+insert into business_area(business_type,business_name,latitude,longitude)
+values("터미널","대전서남부터미널","36.312983","127.388103");
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -11,3 +11,14 @@ CREATE TABLE IF NOT EXISTS users(
     auth VARCHAR(20) COMMENT '회원 권한',
     create_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '가입 날짜'
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 주변 상권 테이블
+CREATE TABLE IF NOT EXISTS business_area(
+	business_no INTEGER(4) AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '상권 번호',
+	business_type VARCHAR(30) COMMENT '병원, 마트, 지하철',
+	business_name VARCHAR(40) COMMENT '상권 이름',
+	latitude VARCHAR(40) COMMENT '위도',
+	longitude VARCHAR(40) COMMENT '경도',
+	create_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '데이터 생성 날짜'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
