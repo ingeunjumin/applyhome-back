a3aefa239210a71120d26197a170f1c9eef30c37
dkdmlk <dkdmlk@naver.com>
2022-07-07 09:21:25 +0900

    feat : 코드정리

--- src/main/java/com/ingeunjumin/project/service/StatisticsService.java
+++ src/main/java/com/ingeunjumin/project/service/StatisticsService.java
@@ -1,7 +1,6 @@
 package com.ingeunjumin.project.service;
 
 
-import java.sql.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -26,8 +25,6 @@ public class StatisticsService {
 	public List<Map<String,Object>> getSelectMonthTradeCountAndAvgSalesAmount(){
 		String startDate = "2021-06";
 		String endDate = "2021-11";
-		List<Map<String,Object>> list = statisticsMapper.selectMonthTradeCountAndAvgSalesAmount(startDate, endDate);
-		System.out.println(list.size());
 		return statisticsMapper.selectMonthTradeCountAndAvgSalesAmount(startDate, endDate);
 	}
 	
